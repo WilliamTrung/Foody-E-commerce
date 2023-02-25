018d0886998aaf48cdb7661da8a970d7f1175538
WilliamTrung <williamthanhtrungq2@gmail.com>
2023-02-25 08:55:00 +0700

    Update

---
+++ FoodyWebApplication/Controllers/RoleController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ApplicationCore;
+using ApplicationCore.Models;
+using BusinessService.UnitOfWork;
+
+namespace FoodyWebApplication.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RoleController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork= unitOfWork;
+        }
+
+        // GET: Role
+        public async Task<IActionResult> Index()
+        {
+            var list = await _unitOfWork.RoleService.Get();
+              return View(list);
+        }
+
+        // GET: Role/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _unitOfWork.RoleService.GetFirst(c => c.Id == id);                
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+
+        // GET: Role/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Role/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.RoleService.Add(role);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(role);
+        }
+
+        // GET: Role/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _unitOfWork.RoleService.GetFirst(c => c.Id == id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Role/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Role role)
+        {
+            if (id != role.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _unitOfWork.RoleService.Update(role);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return NotFound();
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(role);
+        }
+
+        // GET: Role/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _unitOfWork.RoleService.GetFirst(c => c.Id == id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+
+        // POST: Role/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var role = await _unitOfWork.RoleService.GetFirst(c => c.Id == id); ;
+            if (role != null)
+            {
+                await _unitOfWork.RoleService.Delete(role);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
